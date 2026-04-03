c0b1c337d96348ede5e3e7c8853bbe5cdd9fdb37
silason1 <anania.augusto@statementmc.com>
2026-04-03 22:53:57 +0100

    feat: add user output into otp validation

--- src/main/java/com/boleia/boleia/shared/application/ValidationOtp.java
+++ src/main/java/com/boleia/boleia/shared/application/ValidationOtp.java
@@ -2,6 +2,8 @@ package com.boleia.boleia.shared.application;
 
 import org.springframework.stereotype.Service;
 
+import com.boleia.boleia.entity.domain.UserOutput;
+import com.boleia.boleia.entity.domain.UserRepository;
 import com.boleia.boleia.shared.domain.notification.OtpExpiredError;
 import com.boleia.boleia.shared.domain.notification.OtpNotFoundError;
 import com.boleia.boleia.shared.domain.notification.OtpNotMatchError;
@@ -18,20 +20,34 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class ValidationOtp {
     private final InMemoryOtpRepository repository;
+    private final UserRepository userRepository;
 
-    public Result<Boolean, DomainError> execute(ValidationOtpInput input){
+    public Result<UserOutput, DomainError> execute(ValidationOtpInput input){
 
         if(input.phoneNumber() == null || input.otp() == null) return Result.error(new PayloadIncorrectError());
 
         var entryOrErr = repository.findByPhoneNumber(input.phoneNumber());
         if(entryOrErr.unwrap() == null) return Result.error(new OtpNotFoundError());
         if(entryOrErr.unwrap().isExpired()) return Result.error(new OtpExpiredError());
+
+        var userOrErr = this.userRepository.findByPhoneNumber(input.phoneNumber());
+        if(userOrErr.isError()) return Result.error(userOrErr.unwrapError());
         
         boolean valid = entryOrErr.unwrap().getOtp().equals(input.otp());
         
         if(!valid) return Result.error(new OtpNotMatchError());
 
-        return Result.ok(valid);
+        var out = new UserOutput(
+            userOrErr.unwrap().getId().toString(), 
+            userOrErr.unwrap().getFirstName(), 
+            userOrErr.unwrap().getLastName(), 
+            userOrErr.unwrap().getPhoneNumber(), 
+            false, 
+            null,
+            null
+        );
+
+        return Result.ok(out);
 
     }
     
