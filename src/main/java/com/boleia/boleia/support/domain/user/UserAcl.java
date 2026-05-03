93cbf03e217f3f9dd5a396c81d9b03bfc5066961
silason1 <anania.augusto@statementmc.com>
2026-05-03 14:27:55 +0100

    feat: add user acl

--- src/main/java/com/boleia/boleia/support/domain/user/UserAcl.java
+++ src/main/java/com/boleia/boleia/support/domain/user/UserAcl.java
@@ -1,9 +1,9 @@
 package com.boleia.boleia.support.domain.user;
 
-import java.util.UUID;
-
 import com.boleia.boleia.shared.types.Result;
 
-public interface UserAcl {
+import java.util.UUID;
+
+public interface UserACL {
     Result<User, UserNotFoundError> findById(UUID id);
 }
---
+++ src/main/java/com/boleia/boleia/support/infra/acl/UserACLService.java
@@ -0,0 +1,37 @@
+package com.boleia.boleia.support.infra.acl;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.boleia.boleia.shared.jpa.entity.UserModel;
+import com.boleia.boleia.shared.jpa.entity.UserModelJpa;
+import com.boleia.boleia.shared.types.Result;
+import com.boleia.boleia.support.domain.user.User;
+import com.boleia.boleia.support.domain.user.UserACL;
+import com.boleia.boleia.support.domain.user.UserNotFoundError;
+
+import lombok.RequiredArgsConstructor;
+
+@Service("support.UserACL")
+@RequiredArgsConstructor
+public class UserACLService implements UserACL {
+    private final UserModelJpa jpa;
+
+    @Override
+    public Result<User, UserNotFoundError> findById(UUID id) {
+        var model = this.jpa.findById(id.toString());
+        return model.isPresent()
+            ? Result.ok(this.toUserFactory(model.get()))
+            : Result.error(null);
+    }
+
+    public User toUserFactory(UserModel model){
+        return new User(
+            UUID.fromString(model.getId()),
+            model.getFirstName(),
+            model.getLastName()
+        );
+    }
+
+}
