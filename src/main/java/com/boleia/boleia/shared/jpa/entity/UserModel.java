f9abd7e178477bf54802b8dc9d59cd3dec66c17e
silason1 <anania.augusto@statementmc.com>
2026-05-03 16:31:19 +0100

    fix: fix chat support and add new migration

--- src/main/java/com/boleia/boleia/shared/jpa/entity/UserModel.java
+++ src/main/java/com/boleia/boleia/shared/jpa/entity/UserModel.java
@@ -48,13 +48,3 @@ public class UserModel extends BaseModel {
     }
 
 }
-
-
-    // create table chat_support (
-    //     id varchar(255) not null,
-    //     created_at timestamp(6) not null,
-    //     updated_at timestamp(6) not null,
-    //     message text,
-    //     user_id varchar(255),
-    //     primary key (id)
-    // );
