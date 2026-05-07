1112108050521772560256922e8f87c792fa04d4
silason1 <anania.augusto@statementmc.com>
2026-05-07 22:19:09 +0100

    feat: atribute user control

--- src/main/java/com/boleia/boleia/entity/infra/http/UserController.java
+++ src/main/java/com/boleia/boleia/entity/infra/http/UserController.java
@@ -4,9 +4,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.boleia.boleia.entity.application.AtributePassword;
+import com.boleia.boleia.entity.application.ChangePassword;
+import com.boleia.boleia.entity.application.RecoveryPassword;
 import com.boleia.boleia.entity.application.RegisterUser;
 import com.boleia.boleia.entity.application.UserFinder;
+import com.boleia.boleia.entity.application.auth.SignIn;
+import com.boleia.boleia.entity.domain.NonMatchPasswordError;
+import com.boleia.boleia.entity.domain.PasswordIsWrongError;
+import com.boleia.boleia.entity.domain.PasswordLengthError;
+import com.boleia.boleia.entity.domain.PasswordMustBeNumbersAndHaveSixDigitsError;
 import com.boleia.boleia.entity.domain.PhoneNumberIsAlreadyExistsError;
+import com.boleia.boleia.entity.domain.RawAndPasswordMustProvidedError;
+import com.boleia.boleia.entity.domain.SignInOutput;
+import com.boleia.boleia.entity.domain.UserIsAlreadyBanedError;
+import com.boleia.boleia.entity.domain.UserIsAlreadyDeactivatedError;
+import com.boleia.boleia.entity.domain.UserIsAlreadyDeclinedError;
+import com.boleia.boleia.entity.domain.UserIsAlreadyDelitedError;
+import com.boleia.boleia.entity.domain.UserIsAlreadyPendingError;
 import com.boleia.boleia.entity.domain.UserNotFoundError;
 import com.boleia.boleia.entity.domain.UserOutput;
 import com.boleia.boleia.shared.types.HttpResponse;
@@ -23,6 +38,7 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -35,8 +51,11 @@ public class UserController {
 
     private final UserFinder finder;
     private final RegisterUser registerUser;
-    // private final AddFirebaseTokenMessage addFirebaseTokenMessage;
     private final UserInputMapper inputMapper;
+    private final ChangePassword changePassword;
+    private final AtributePassword atributePassword;
+    private final RecoveryPassword recoveryPassword;
+    private final SignIn signin;
     
     @PostMapping("/user")
     @Operation(
@@ -106,6 +125,110 @@ public class UserController {
         return ResponseEntity.ok(out.unwrap());
     }
 
+
+
+    @PostMapping("/auth/user/signin")
+    @Operation(
+        summary = "Sign in into system",
+        responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(name = "SigInOutput",implementation = SignInOutput.class))),
+            @ApiResponse(responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(name = "ErrorResponse",implementation = HttpResponse.class))),
+            @ApiResponse(responseCode = "404",content = @Content(mediaType = "application/json",schema = @Schema(name = "ErrorResponse",implementation = HttpResponse.class))),
+        }
+    )
+    public ResponseEntity<?> signIn(@RequestBody SignInRequest body) {
+
+        var input = inputMapper.toSignInInput(body);
+        var out = signin.execute(input);
+
+        if(out.isError() && out.unwrapError().getClass().equals(UserNotFoundError.class)) return HttpResponse.notFound(out.unwrapError().getMsg());
+        
+        if(out.isError() && out.unwrapError().getClass().equals(UserIsAlreadyDeactivatedError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        if(out.isError() && out.unwrapError().getClass().equals(UserIsAlreadyBanedError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        if(out.isError() && out.unwrapError().getClass().equals(UserIsAlreadyDeclinedError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        if(out.isError() && out.unwrapError().getClass().equals(UserIsAlreadyPendingError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        if(out.isError() && out.unwrapError().getClass().equals(UserIsAlreadyDelitedError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+
+        if(out.isError() && out.unwrapError().getClass().equals(PasswordIsWrongError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        if(out.isError() && out.unwrapError().getClass().equals(RawAndPasswordMustProvidedError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+
+
+        return ResponseEntity.ok(out.unwrap());
+    }
+    
+    @PatchMapping("/auth/user/atribute-password")
+    @Operation(
+        summary = "Atribute a password in a user",
+        responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(name = "",implementation = Boolean.class))),
+            @ApiResponse(responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(name = "ErrorResponse",implementation = HttpResponse.class))),
+            @ApiResponse(responseCode = "404",content = @Content(mediaType = "application/json",schema = @Schema(name = "ErrorResponse",implementation = HttpResponse.class))),
+        }
+    )
+    public ResponseEntity<?> atributePassword(@RequestBody AtributePasswordRequest body) {
+
+        var input = inputMapper.toAtributePasswordInput(body);
+        var out = atributePassword.execute(input);
+
+        if(out.isError() && out.unwrapError().getClass().equals(UserNotFoundError.class)) return HttpResponse.notFound(out.unwrapError().getMsg());
+
+        if(out.isError() && out.unwrapError().getClass().equals(PasswordIsWrongError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        
+        if(out.isError() && out.unwrapError().getClass().equals(RawAndPasswordMustProvidedError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        if(out.isError() && out.unwrapError().getClass().equals(PasswordMustBeNumbersAndHaveSixDigitsError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        if(out.isError() && out.unwrapError().getClass().equals(PasswordLengthError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+
+        return ResponseEntity.ok(out.unwrap());
+    }
+
+
+    @PatchMapping("/auth/user/recovery-password")
+    @Operation(
+        summary = "Recovery a password in a user",
+        responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(name = "",implementation = Boolean.class))),
+            @ApiResponse(responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(name = "ErrorResponse",implementation = HttpResponse.class))),
+            @ApiResponse(responseCode = "404",content = @Content(mediaType = "application/json",schema = @Schema(name = "ErrorResponse",implementation = HttpResponse.class))),
+        }
+    )
+    public ResponseEntity<?> recoveryPassword(@RequestBody RecoveryPasswordRequest body) {
+
+        var input = inputMapper.toRecoveryPassworInput(body);
+        var out = recoveryPassword.execute(input);
+
+        if(out.isError() && out.unwrapError().getClass().equals(UserNotFoundError.class)) return HttpResponse.notFound(out.unwrapError().getMsg());
+
+        if(out.isError() && out.unwrapError().getClass().equals(PasswordIsWrongError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        
+        if(out.isError() && out.unwrapError().getClass().equals(RawAndPasswordMustProvidedError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        if(out.isError() && out.unwrapError().getClass().equals(PasswordMustBeNumbersAndHaveSixDigitsError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+        if(out.isError() && out.unwrapError().getClass().equals(PasswordLengthError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+
+        return ResponseEntity.ok(out.unwrap());
+    }
+
+    @PatchMapping("/driver/{id}/change-password")
+    @Operation(
+        summary = "Change driver password",
+        responses = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(name = "DriverOutput"))),
+            @ApiResponse(responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(name = "ErrorResponse",implementation = HttpResponse.class))),
+            @ApiResponse(responseCode = "404",content = @Content(mediaType = "application/json",schema = @Schema(name = "ErrorResponse",implementation = HttpResponse.class))),
+        }
+    )
+    public ResponseEntity<?> changeDriverPassword(@RequestBody ChangePasswordRequest body) {
+
+        var input = inputMapper.toChangePasswordInput(body);
+        var out = changePassword.execute(input);
+
+        if(out.isError() && out.unwrapError().getClass().equals(UserNotFoundError.class)) return HttpResponse.notFound(out.unwrapError().getMsg());
+
+        if(out.isError() && out.unwrapError().getClass().equals(NonMatchPasswordError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+
+        if(out.isError() && out.unwrapError().getClass().equals(PasswordIsWrongError.class)) return HttpResponse.badRequest(out.unwrapError().getMsg());
+
+        return ResponseEntity.ok(null);
+    }
 
     // @PatchMapping("/user/fcm")
     // @Operation(
--- src/main/java/com/boleia/boleia/entity/infra/http/UserInputMapper.java
+++ src/main/java/com/boleia/boleia/entity/infra/http/UserInputMapper.java
@@ -2,12 +2,32 @@ package com.boleia.boleia.entity.infra.http;
 
 import org.springframework.stereotype.Component;
 
+import com.boleia.boleia.entity.application.AtributePasswordInput;
+import com.boleia.boleia.entity.application.ChangePasswordInput;
+import com.boleia.boleia.entity.application.RecoveryPasswordInput;
 import com.boleia.boleia.entity.application.RegisterUserInput;
+import com.boleia.boleia.entity.application.auth.SignInInput;
 
 @Component
 public class UserInputMapper {
     
     public RegisterUserInput toRegisterUserInput(RegisterUserRequest body) {
         return new RegisterUserInput(body.firstName(), body.lastName(), body.phoneNumber(), body.isDriver());
+    }
+
+    public ChangePasswordInput toChangePasswordInput(ChangePasswordRequest body) {
+        return new ChangePasswordInput(body.driverId(), body.oldPassword(), body.confirmedPassword());
+    }
+
+    public SignInInput toSignInInput(SignInRequest body) {
+        return new SignInInput(body.phoneNumber(), body.password());
+    }
+
+    public AtributePasswordInput toAtributePasswordInput(AtributePasswordRequest body) {
+        return new AtributePasswordInput(body.identificationNumber(), body.password());
+    }
+
+    public RecoveryPasswordInput toRecoveryPassworInput(RecoveryPasswordRequest body) {
+        return new RecoveryPasswordInput(body.phoneNumber(), body.password());
     }
 }
